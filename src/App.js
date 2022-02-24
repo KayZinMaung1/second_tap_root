dff7f98e4b1792159ae0ec31f5e3d1cfeea1f1dd
KayZinMaung1 <kayzinmg363@gmail.com>
2022-02-24 13:33:32 +0630

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import Home from './pages/Home';
+import { BrowserRouter as Router,Route,Routes } from 'react-router-dom';
+
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <p>hi</p>
       <Navbar/>
+      <div className='content'>
+       <Routes>
+       <Route path="/" element={<Home/>}></Route>
+       {/* <Route path="/about" element={<About/>}></Route>
+       <Route path="/blogs" element={<Blogs/>}></Route>
+       <Route path="/gallery" element={<Gallery/>}></Route> */}
+       </Routes>
+      </div>
+   
     </div>
+    </Router>
   );
 }
 
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import styles from './Navbar.module.css';
-import { Button } from "../Button";
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
     const[clicked,setClicked] = useState(false);
     const handleClick =()=>{
@@ -8,28 +9,44 @@ const Navbar = () => {
     }
     return ( 
         <nav className={styles.NavbarItems}>
-            <h1 className={styles.navbar_logo}>React<i className="fab fa-react"></i></h1>
+            <div className={styles.navlogopostition}>
+                    <Link to="/" className={styles.link}>
+                        <img src={process.env.PUBLIC_URL + 'images/logo.jpg'} alt="Logo" width={50} height={50} className={styles.navlogo}/>
+                        <p className={styles.nav_logo_text}>
+                        Second Tap Root <br></br>
+                        ဒုတိယရေသောက်မြစ်</p>
+                    </Link>
+            </div>
             <div className={styles.menu_icon} onClick={handleClick}>
                 <i className={clicked? 'fas fa-times' :'fas fa-bars'}></i>
             </div>
             <ul className={clicked ? [styles.nav_menu, styles.nav_menu_active].join(" ") : styles.nav_menu}>
                 <li>
-                    <a className={styles.nav_links}>Home</a>
+                    <Link to="/" className={styles.link}>
+                    <p className={styles.nav_links} >Home</p>
+                    </Link>
                 </li>
                 <li>
-                    <a className={styles.nav_links}>About Us</a>
+                    <Link to="/" className={styles.link}>
+                    <p className={styles.nav_links} >About Us</p>
+                    </Link>
                 </li>
                 <li>
-                    <a className={styles.nav_links}>Blogs</a>
+                <Link to="/" className={styles.link}>
+                    <p className={styles.nav_links} >Blogs</p>
+                    </Link>
                 </li>
                 <li>
-                    <a className={styles.nav_links}>Gallery</a>
+                <Link to="/" className={styles.link}>
+                    <p className={styles.nav_links} >Gallery</p>
+                    </Link>
                 </li>
                 <li>
-                    <a className={styles.nav_links_mobile}>Contact Us</a>
+                <Link to="/" className={styles.link}>
+                    <p className={styles.nav_links_mobile} >Contact Us</p>
+                    </Link>
                 </li>
             </ul>
-            <Button>Sign Up</Button>
         </nav>
      );
 }
